c94443923422f2376449667a859155f42c8dafad
RafaBaker <rafael1309mt@gmail.com>
2024-03-25 22:32:24 -0300

    ldep4

--- LDEP1_4/codigo_quebrado.c
+++ LDEP1_4/codigo_quebrado.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
-int main()
-  int i=0, den=0, primo=0, qtd=0, cont=0;
+int main() {
+  int i=0, div=1, qtd=0, cont=0;
 
   scanf("%d", &qtd);
 
-  for(i=2; i<qtd; i++){
-    while(den < i){
-      if(i%den==0){
+  for (i=2; i<=qtd; i++) {
+    while (div <= i) {
+      if ((i % div) == 0) {
         cont++;
       }
+      div++;
     }
-    if(cont==2)
-      printf("%d\n", i);
+    
+    if (cont==2) {
+      printf("%d ", i);
+    }
+    
+    cont = 0;
+    div = 1;
   }
 
   return 0;
+}
