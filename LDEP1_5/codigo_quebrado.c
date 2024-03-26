e34bf9e90adb45b9e3b143e68d127f4fc08a4ed7
RafaBaker <rafael1309mt@gmail.com>
2024-03-25 23:01:27 -0300

    esqueci o principal rsrs

--- LDEP1_5/codigo_quebrado.c
+++ LDEP1_5/codigo_quebrado.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	int a, b, den=1, i=0, mdc=0, mmc=0, limMDC, limMMC;
+	int a, b, den=1, i=0, mdc=0, mmc=1, limMDC, limMMC;
 
 	scanf("%d %d", &a, &b);
 
@@ -16,22 +16,28 @@ int main(){
 	}
 	
 	//MDC
-	for(den=1; den <= limMDC; den+1){   
-		if(!(a%den) || !(b%den)){ 
+	for(den=1; den <= limMDC; den++){   
+		if((a%den == 0) && (b%den == 0)){ 
 			mdc = den;
 		}
 	}
 	
 	//MMC
-	i = limMMC;
-	while(1){
-		if(!(i%a) || !(i%b))      
-			mmc = i;
-			break;
-		i++;                      
-	}
+	// i = limMMC;
+	// while(1){
+	// 	if (mdc == 1) {
+	// 		mmc = a*b; 
+	// 		break;
+	// 	}
+	// 	else if ((i%a == 0) && (i%b == 0)) {      
+	// 		mmc = i;
+	// 		break;
+	// 	i++;
+	// 	}
+	// }
+	mmc = a * (b/mdc);
 
-	printf("%d %d", mmc, mdc);
+	printf("%d %d", mdc, mmc);
 
 	return 0;
 }
