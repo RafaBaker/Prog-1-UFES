db81f489e8050da97d8159a788f0c2b55775f703
RafaBaker <rafael1309mt@gmail.com>
2024-03-28 14:34:46 -0300

    consegui resolver o erro, porém não passou no BOCA .........

--- L2_15/l2_15.c
+++ L2_15/l2_15.c
@@ -3,13 +3,10 @@
 int main () {
 
     int num = 0, qtdpos = 0, qtdneg = 0, somaneg = 0, somapos = 0;
-    char espaco = ' ';
+    char espaco = '\0';
 
-    scanf("%d", &num);
-    // printf("%d\n", num);
-    // scanf("%c", &espaco);
-    // printf("%d\n", espaco);
-    while (scanf("%c[^\0]", &espaco)) {
+    while ((scanf("%d", &num)) == 1) {
+        // scanf("%d%c", &num, &espaco);
         if (num > 0) {
             somapos+=num;
             qtdpos++;
@@ -18,30 +15,11 @@ int main () {
             somaneg += num;
             qtdneg++;
         }
-        espaco = 1;
-        scanf("%d", &num);
-        // printf("%d\n", num);
-        // scanf("%c", &espaco);
-        // printf("%d\n", espaco);
     }
-    // if (num > 0) {
-    //     somapos+=num;
-    //     qtdpos++;
-    //     }
-    // else {
-    //     somaneg += num;
-    //     qtdneg++;
-    // }
 
-    printf("%d %d %d %d", qtdneg, qtdpos, somaneg, somapos);
-
-    // while (1) {
-    //     scanf("%d%c", &num, &espaco);
-    //     printf("%d\n", espaco);
-    //     if (espaco == '=' || espaco == '0') break;
-    // }
-    // scanf("%c", &espaco);
-    // printf("%d", espaco);
+    if (!((qtdneg == 0) && (qtdpos == 0) && (somaneg == 0) && (somapos==0))) {
+        printf("%d %d %d %d", qtdneg, qtdpos, somaneg, somapos);
+    }
 
     return 0;
 }
