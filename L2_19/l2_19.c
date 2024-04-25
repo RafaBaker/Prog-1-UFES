068f4ff7e3846bee4aa9b47766e292fd54c49430
RafaBaker <rafael1309mt@gmail.com>
2024-04-25 13:38:37 -0300

    colocando todas as l2

--- L2_19/l2_19.c
+++ L2_19/l2_19.c
@@ -3,14 +3,16 @@
 int main () {
     int qtd, id, maiorID = 0, maiorM = 0, maior2M = 0, maior3M = 0, maiorF = 0, maior2F = 0, maior3F = 0;
     int nota1, nota2, nota3;
+    int maiornota1A, maiornota2A, maiornota3A;
     char sexo;
     int i = 0;
 
     scanf("%d", &qtd);
     for (;i < qtd; i++) {
         scanf("%d %c %d %d %d", &id, &sexo, &nota1, &nota2, &nota3);
+        // Organizar as notas em ordem e armarzenar elas em maiornota1A, maiornota2A, maiornota3A
+        
         if (sexo == 'M') {
-
         }
     }
 
