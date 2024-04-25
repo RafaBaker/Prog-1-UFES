5f834813618f943d534d97ac8cc27267f37c4b5b
RafaBaker <rafael1309mt@gmail.com>
2024-04-25 12:28:39 -0300

    tentando fazer esse

---
+++ L2_19/l2_19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+    int qtd, id, maiorID = 0, maiorM = 0, maior2M = 0, maior3M = 0, maiorF = 0, maior2F = 0, maior3F = 0;
+    int nota1, nota2, nota3;
+    char sexo;
+    int i = 0;
+
+    scanf("%d", &qtd);
+    for (;i < qtd; i++) {
+        scanf("%d %c %d %d %d", &id, &sexo, &nota1, &nota2, &nota3);
+        if (sexo == 'M') {
+
+        }
+    }
+
+    return 0;
+}
