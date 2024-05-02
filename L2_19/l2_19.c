dd45f09f273c70bf97cb492eeddc30b5ce01deb7
RafaBaker <rafael1309mt@gmail.com>
2024-05-01 21:54:47 -0300

    ACREDITO que terminada a l2_19

--- L2_19/l2_19.c
+++ L2_19/l2_19.c
@@ -1,71 +1,14 @@
 #include <stdio.h>
 
-int maiorValor (int a, int b, int c) {
-    int maior = 0;
-
-    if (a > maior) {
-        maior = a;
-    }
-    
-    if (b > maior) {
-        maior = b;
-    }
-
-    if (c > maior) {
-        maior = c;
-    }
-
-    return maior;
-}
-
-int menorValor(int a, int b, int c) {
-    int menor = 11;
-
-    if (a < menor) {
-        menor = a;
-    }
-    
-    if (b < menor) {
-        menor = b;
-    }
-
-    if (c < menor) {
-        menor = c;
-    }
-
-    return menor;
-}
-
-int valorMeio (int a, int b, int c) {
-    int meio = 0, maior = 0, menor = 0;
-
-    maior = maiorValor(a, b, c);
-    menor = menorValor(a, b, c);
-
-    if (!(a == menor || a == maior)) {
-        meio = a;
-    }
-
-    if (!(b == menor || b == maior)) {
-        meio = b;
-    }
-
-    if (!(c == menor || c == maior)) {
-        meio = c;
-    }
-
-    return meio;
-}
-
 int main () {
-    int qtd, id, maiorIDM = 0, maiorIDF = 0, maiorM = 0, maior2M = 0, maior3M = 0,
+    int qtd = 0, id = 0, maiorIDM = 0, maiorIDF = 0, maiorM = 0, maior2M = 0, maior3M = 0,
      maiorF = 0, maior2F = 0, maior3F = 0;
     int piorIDM = 1, piorNotaM1 = 11, piorNotaM2 = 11, piorNotaM3 = 11, piorIDF = -1, piorNotaF1 = 11, piorNotaF2 = 11, piorNotaF3 = 11;
-    int nota1, nota2, nota3;
-    int maiornota1A, maiornota2A, maiornota3A;
+    int nota1 = 0, nota2 = 0, nota3 = 0;
+    // int nota1 = 0, nota2 = 0, nota3 = 0;
     int somaNotasM = 0, somaNotasF = 0, delegCampeaoNotasM = 0, delegCampeaoIDM = 0, delegCampeaoNotasF = 0, delegCampeaoIDF = 0;
     char sexo;
-    int i = 1;
+    int i = 1, tmp = 0;
 
     scanf("%d", &qtd);
     for (;i <= qtd; i++) {
@@ -75,86 +18,104 @@ int main () {
             if (id == -1) {
                 break;
             }
-            scanf("%c %d %d %d", &sexo, &nota1, &nota2, &nota3);
-            // Organizar as notas em ordem e armarzenar elas em maiornota1A, maiornota2A, maiornota3A
-            maiornota1A = maiorValor(nota1, nota2, nota3);
-            maiornota2A = menorValor(nota1, nota2, nota3);
-            maiornota3A = valorMeio(nota1, nota2, nota3);
+            scanf(" %c %d %d %d", &sexo, &nota1, &nota2, &nota3);
+            // Organizar as notas em ordem e armarzenar elas em nota1, nota2, nota3
+            // nota1 = maiorValor(nota1, nota2, nota3);
+            // nota2 = valorMeio(nota1, nota2, nota3);
+            // nota3 = menorValor(nota1, nota2, nota3);
+            // organizando as notas em ordem crescente
+            if (nota1 < nota2) {
+                tmp = nota2;
+                nota2 = nota1;
+                nota1 = tmp;
+            }
+            if (nota1 < nota3) {
+                tmp = nota3;
+                nota3 = nota1;
+                nota1 = tmp;
+            }
+            if (nota2 < nota3) {
+                tmp = nota3;
+                nota3 = nota2;
+                nota2 = tmp;
+            }
 
             if (sexo == 'M') {
-                somaNotasM += maiornota1A;
-                if (maiornota1A > maiorM) {
-                    maiorM = maiornota1A;
-                    maior2M = maiornota2A;
-                    maior3M = maiornota3A;
+                somaNotasM += nota1;
+                if (nota1 > maiorM) {
+                    maiorM = nota1;
+                    maior2M = nota2;
+                    maior3M = nota3;
                     maiorIDM = id;
-                } else if (maiornota1A == maiorM) {
-                    if (maiornota2A > maior2M) {
-                        maior2M = maiornota2A;
-                        maior3M = maiornota3A;
+                } else if (nota1 == maiorM) {
+                    if (nota2 > maior2M) {
+                        maior2M = nota2;
+                        maior3M = nota3;
                         maiorIDM = id;
-                    } else if (maiornota2A == maior2M) {
-                        if (maiornota3A > maior3M) {
-                            maior3M = maiornota3A;
+                    } else if (nota2 == maior2M) {
+                        if (nota3 > maior3M) {
+                            maior3M = nota3;
                             maiorIDM = id;
-                        } else if (maiornota3A > maior3M) {
+                        } else if (nota3 > maior3M) {
                             if (id < maiorIDM) maiorIDM = id;
                         }
                     } 
-                } else if (maiornota1A < piorNotaM1) {
-                    piorNotaM1 = maiornota1A;
-                    piorNotaM2 = maiornota2A;
-                    piorNotaM3 = maiornota3A;
+                } 
+                if (nota1 < piorNotaM1) {
+                    piorNotaM1 = nota1;
+                    piorNotaM2 = nota2;
+                    piorNotaM3 = nota3;
                     piorIDM = id;
-                } else if (maiornota1A == piorNotaM1) {
-                    if (maiornota2A < piorNotaM2) {
-                        piorNotaM2 = maiornota2A;
-                        piorNotaM3 = maiornota3A;
+                } else if (nota1 == piorNotaM1) {
+                    if (nota2 < piorNotaM2) {
+                        piorNotaM2 = nota2;
+                        piorNotaM3 = nota3;
                         piorIDM = id;
-                    } else if (maiornota2A == piorNotaM2) {
-                        if (maiornota3A < piorNotaM3) {
-                            piorNotaM3 = maiornota3A;
+                    } else if (nota2 == piorNotaM2) {
+                        if (nota3 < piorNotaM3) {
+                            piorNotaM3 = nota3;
                             piorIDM = id;
-                        } else if (maiornota3A == piorNotaM3) {
+                        } else if (nota3 == piorNotaM3) {
                             if (id > piorIDM) piorIDM = id;
                         }
                     }
                 }
             } else {
-                somaNotasF += maiornota1A;
-                if (maiornota1A > maiorF) {
-                    maiorF = maiornota1A;
-                    maior2F = maiornota2A;
-                    maior3F = maiornota3A;
+                somaNotasF += nota1;
+                if (nota1 > maiorF) {
+                    maiorF = nota1;
+                    maior2F = nota2;
+                    maior3F = nota3;
                     maiorIDF = id;
-                } else if (maiornota1A == maiorF) {
-                    if (maiornota2A > maior2F) {
-                        maior2F = maiornota2A;
-                        maior3F = maiornota3A;
+                } else if (nota1 == maiorF) {
+                    if (nota2 > maior2F) {
+                        maior2F = nota2;
+                        maior3F = nota3;
                         maiorIDF = id;
-                    } else if (maiornota2A == maior2F) {
-                        if (maiornota3A > maior3F) {
-                            maior3F = maiornota3A;
+                    } else if (nota2 == maior2F) {
+                        if (nota3 > maior3F) {
+                            maior3F = nota3;
                             maiorIDF = id;
-                        } else if (maiornota3A > maior3F) {
+                        } else if (nota3 > maior3F) {
                             if (id < maiorIDF) maiorIDF = id;
                         }
                     } 
-                } else if (maiornota1A < piorNotaF1) {
-                    piorNotaF1 = maiornota1A;
-                    piorNotaF2 = maiornota2A;
-                    piorNotaF3 = maiornota3A;
+                } 
+                if (nota1 < piorNotaF1) {
+                    piorNotaF1 = nota1;
+                    piorNotaF2 = nota2;
+                    piorNotaF3 = nota3;
                     piorIDF = id;
-                } else if (maiornota1A == piorNotaF1) {
-                    if (maiornota2A < piorNotaF2) {
-                        piorNotaF2 = maiornota2A;
-                        piorNotaF3 = maiornota3A;
+                } else if (nota1 == piorNotaF1) {
+                    if (nota2 < piorNotaF2) {
+                        piorNotaF2 = nota2;
+                        piorNotaF3 = nota3;
                         piorIDF = id;
-                    } else if (maiornota2A == piorNotaF2) {
-                        if (maiornota3A < piorNotaF3) {
-                            piorNotaF3 = maiornota3A;
+                    } else if (nota2 == piorNotaF2) {
+                        if (nota3 < piorNotaF3) {
+                            piorNotaF3 = nota3;
                             piorIDF = id;
-                        } else if (maiornota3A == piorNotaF3) {
+                        } else if (nota3 == piorNotaF3) {
                             if (id > piorIDF) piorIDF = id;
                         }
                     }
