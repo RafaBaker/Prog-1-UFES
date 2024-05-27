30ab734f81fc3b8b5f7675e18c1363bd0377722e
RafaBaker <rafael1309mt@gmail.com>
2024-05-27 15:48:47 -0300

    l4_4 feita

--- L4_4/l4_4.c
+++ L4_4/l4_4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct 
 {
@@ -38,23 +39,92 @@ int ObtemOrdenada(tPonto p)
     return p.y;
 }
 
-int Dleta
+int DeltaX(int x1, int x2)
+{
+    return x2-x1;
+}
 
-void LePonto()
+int DeltaY(int y1, int y2)
+{
+    return y2-y1;
+}
+
+int DistanciaPontos(tPonto p1, tPonto p2)
+{
+    int dist = 0, soma = 0;
+    int deltaX, deltaY;
+
+    deltaX = DeltaX(p1.x, p2.x);
+    deltaY = DeltaY(p1.y, p2.y);
+
+    deltaX = pow(deltaX, 2);
+    deltaY = pow(deltaY, 2);
+    soma = deltaX + deltaY;
+
+    dist = sqrt(soma);
+
+    return dist;
+}
+
+int identificaQuadrante(tPonto p)
+{
+    if (p.x == 0 || p.y == 0) return 0;
+    else if (p.x > 0 && p.y > 0) return 1;
+    else if (p.x < 0 && p.y > 0) return 2;
+    else if (p.x < 0 && p.y < 0) return 3;
+    else if (p.x > 0 && p.y < 0) return 4;
+}
+
+tPonto pontoSimetrico(tPonto p)
+{
+    tPonto simetrico;
+
+    simetrico = p;
+    
+    simetrico = AlteraAbscissa(simetrico);
+    simetrico = AlteraOrdenada(simetrico);
+
+    return simetrico;
+}
+
+tPonto LePonto()
 {
     int x, y;
+    tPonto ponto;
 
     scanf("%d %d", &x, &y);
+    ponto.x = x;
+    ponto.y = y;
+
+    return ponto;
 }
 
 void ImprimePonto(tPonto p)
 {
-    printf("(%d,%d)", ponto.x, ponto.y);
+    printf("(%d,%d) ", p.x, p.y);
 }
 
 int main ()
 {
-    tPonto p;
+    tPonto p, pSim;
+    int n = 0, i;
+    int quadrante = -1;
+
+    scanf("%d", &n);
+    for (i = 0; i < n;i++) 
+    {
+        p = LePonto();
+        ImprimePonto(p);
+        quadrante = identificaQuadrante(p);
+        printf("%d ", quadrante);
+
+        pSim = pontoSimetrico(p);
+        ImprimePonto(pSim);
+        quadrante = identificaQuadrante(pSim);
+        printf("%d", quadrante);
+
+        printf("\n");
+    }
 
     return 0;
 }
