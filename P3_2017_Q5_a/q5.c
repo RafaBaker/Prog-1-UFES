a2025b707fd2b961d967e34dbd790568c43dd1b8
RafaBaker <rafael1309mt@gmail.com>
2024-06-11 15:33:42 -0300

    ajudando a amiga

---
+++ P3_2017_Q5_a/q5.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+void ImprimeSequencia (int n, int seq[]);
+
+void DeslocaDireita(int num, int seq[], int tam);
+
+int main ()
+{
+    int n1 = 0, n2 = 0, i;
+    
+    
+    while (scanf("%d", &n1) == 1)
+    {
+        int seq[n1];
+
+        for (i = 0; i < n1; i++)
+        {
+            scanf("%d", &seq[i]);
+        }
+
+        while (1)
+        {
+            scanf("%d", &n2);
+            if (n2 == -1) break;
+
+            DeslocaDireita(n2, seq, n1);
+        }
+    }
+    return 0;
+}
+
+void DeslocaDireita(int num, int seq[], int tam)
+{
+    int i = 0, j = 0, aux = 0;
+    int vet[tam];
+    int soma = 0;
+    for (i = 0; i < tam; i++)
+    {
+        soma = i+num;
+        if (soma >= tam)
+        {
+            soma = soma % tam;
+        }
+        vet[soma] = seq[i];
+    }
+
+    //copiando o vetor pro outro
+    for (i = 0; i < tam; i++)
+    {
+        seq[i] = vet[i];
+    }
+
+    ImprimeSequencia(tam, vet);
+}
+
+void ImprimeSequencia(int n, int seq[])
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", seq[i]);
+        if (i != n-1)
+            printf(" ");
+    }
+    printf("\n");
+}
