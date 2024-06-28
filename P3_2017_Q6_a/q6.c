3aaa0285951163cf0af94ecb42a9b482396b2b9b
RafaBaker <rafael1309mt@gmail.com>
2024-06-28 14:15:01 -0300

    p3 2017 q6

--- P3_2017_Q6_a/q6.c
+++ P3_2017_Q6_a/q6.c
@@ -9,9 +9,10 @@ typedef struct
 
 int main ()
 {
-    int l1, c1;
-    int l2, c2;
+    int l1 = 0, c1 = 0;
+    int l2 = 0, c2 = 0;
     int i, j;
+    int i2, j2;
     
     scanf("%d %d", &l1, &c1);
     int placa1[l1][c1];
@@ -19,7 +20,7 @@ int main ()
     {
         for (j = 0; j < c1; j++)
         {
-            scanf("%d", &placa1[i][j]);
+            scanf("%1d", &placa1[i][j]);
         }
     }
 
@@ -29,7 +30,30 @@ int main ()
     {
         for (j = 0; j < c2; j++)
         {
-            scanf("%d", &placa2[i][j]);
+            scanf("%1d", &placa2[i][j]);
+        }
+    }
+
+    int flag = 1;
+    for (i = 0; i <= l1-l2; i++)
+    {
+        for (j = 0; j <= c1-c2; j++)
+        {
+            flag = 1;
+            //loop pra placa 2
+            for (i2 = 0; i2 < l2 && flag; i2++)
+            {
+                for (j2 = 0; j2 < c2 && flag; j2++)
+                {   
+                    if (placa2[i2][j2] != placa1[i+i2][j+j2])
+                    {
+                        flag = 0;
+                    }
+                }
+            }
+
+            if (flag)
+                printf("%d,%d\n", j, i);
         }
     }
 
